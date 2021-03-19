2cb504976c9ed2def6307d3c84a46bcd70398b04
heavymetalfan1123 <72998757+heavymetalfan1123@users.noreply.github.com>
2021-03-19 20:19:22 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        // ВОТ ТУТ ОБЪЯВЛЯЕМ МАССИВ
+        // НАЗЫВАЕМ ЕГО $emojiArr
+        // И НАПОЛНЯЕМ ЭМОДЖИКАМИ 
+        $emojiArr=['👽','🦄','🎸','👍🏻','🎸','🤑'];
+        $a=$_REQUEST['a'] ?? 2;
+        $mass=[];
+        $res=[];
+
+
+
+
+    ?>
+
+        <?php
+            // здесь выводим случайный элемент массива $emojiArr
+            for ($i=0; $i<$a;$i++){
+                $mass[] = $emojiArr[rand(0,5)];
+                $res[]=rand(0,5);
+            }
+            foreach ($mass as $f) {
+                echo '<div class="block">' . $f . '</div>';
+            }
+            $flag=1;
+            foreach ($res as $r) {
+                if($r!=$res[0]){
+                    $flag=0;
+
+                }
+            }
+            $nov = 0;
+            for ($k = 1; $k < $a; $k++) {
+                if ($mass[$k] == $mass[$k-1]) {
+                    $nov++;
+                }
+            }
+            if ($a - 1 == $nov) {
+                echo "<div id=\"message\">УРА! Ты выиграл!<br></div>";
+            }
+            
+           
+            
+            
+            
+        ?>
+    
+
+    <!-- Кнопка перезагрузки страницы index -->
+    <a href=<?php
+        if ($flag == 1 ){
+            echo "main.php?a=".($a+1);
+        }
+        else{
+            echo "main.php?a=".($a);
+        }
+    ?>
+    class="btn">Играть</a>
+    
+</body>
+</html>
